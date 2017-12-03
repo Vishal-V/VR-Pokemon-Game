abe70d1f6c5b5f11cae800788dc85124e8e81cdf
Vishal Vinod <vishal114186@gmail.com>
2017-12-04 01:13:17 +0530

    Initial commit

---
+++ Scripts/Calculate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculate : MonoBehaviour {
+	public int count;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+
+	public void counterValue(){
+		count = count + 1;
+		Debug.Log ("Count: " + count);
+	}
+
+}
---
+++ Scripts/Trees.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trees : MonoBehaviour {
+
+	public GameObject[] treeRock;
+
+	// Use this for initialization
+	void Start () {
+		for (int j = 0; j < Random.Range(600,700); j++) {
+			treeRockCount ();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void treeRockCount(){
+		int index1 = Random.Range (0, treeRock.Length);
+		GameObject random1 = Instantiate (treeRock [index1]);
+		random1.transform.parent = transform;
+		random1.transform.localPosition = new Vector3 (Random.Range(-99,99), 0.5f, Random.Range(-99,99));
+	}
+}
